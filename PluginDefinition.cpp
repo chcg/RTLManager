28dc979067274c1ebd0b835c30d36300f7b565b8
Ezra Steinmetz <ezra250@gmail.com>
2017-04-09 16:02:58 +0300

    First commit

---
+++ PluginDefinition.cpp
@@ -0,0 +1,170 @@
+//this file is part of notepad++
+//Copyright (C)2003 Don HO <donho@altern.org>
+//
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either
+//version 2 of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#include "PluginDefinition.h"
+#include "menuCmdID.h"
+#include "PrefDialog.h"
+
+//
+// put the headers you need here
+//
+#include <stdlib.h>
+#include <time.h>
+#include <shlwapi.h>
+
+const TCHAR sectionName[] = TEXT("Insert Extesion");
+const TCHAR keyName[] = TEXT("doCloseTag");
+const TCHAR configFileName[] = TEXT("RtlManager.ini");
+bool isEnabled = true;
+
+void toggleRtl();
+#ifdef UNICODE 
+	#define generic_itoa _itow
+#else
+	#define generic_itoa itoa
+#endif
+
+FuncItem funcItem[nbFunc];
+
+//
+// The data of Notepad++ that you can use in your plugin commands
+//
+NppData nppData;
+HelpDialog hDlg;
+PrefDialog pDlg;
+
+
+TCHAR iniFilePath[MAX_PATH];
+
+bool doCloseTag = false;
+
+#define DOCKABLE_DEMO_INDEX 15
+
+//
+// Initialize your plugin data here
+// It will be called while plugin loading   
+void pluginInit(HANDLE hModule)
+{
+	// Initialize dockable demo dialog
+	//_goToLine.init((HINSTANCE)hModule, NULL);
+
+
+}
+
+//
+// Here you can do the clean up, save the parameters (if any) for the next session
+//
+void pluginCleanUp()
+{
+	::WritePrivateProfileString(sectionName, keyName, doCloseTag?TEXT("1"):TEXT("0"), iniFilePath);
+}
+
+void showAboutDialog()
+{
+	hDlg.doDialog();
+}
+
+void showPrefDialog() {
+	pDlg.doDialog();
+}
+//
+// Initialization of your plugin commands
+// You should fill your plugins commands here
+void commandMenuInit()
+{
+	//
+	// Firstly we get the parameters from your plugin config file (if any)
+	//
+
+	// get path of plugin configuration
+	::SendMessage(nppData._nppHandle, NPPM_GETPLUGINSCONFIGDIR, MAX_PATH, (LPARAM)iniFilePath);
+
+	// if config path doesn't exist, we create it
+	if (PathFileExists(iniFilePath) == FALSE)
+	{
+		::CreateDirectory(iniFilePath, NULL);
+	}
+
+	// make your plugin config file full file path name
+	PathAppend(iniFilePath, configFileName);
+
+	// get the parameter value from plugin config
+	doCloseTag = (::GetPrivateProfileInt(sectionName, keyName, 0, iniFilePath) != 0);
+
+
+    //--------------------------------------------//
+    //-- STEP 3. CUSTOMIZE YOUR PLUGIN COMMANDS --//
+    //--------------------------------------------//
+    // with function :
+    // setCommand(int index,                      // zero based number to indicate the order of command
+    //            TCHAR *commandName,             // the command name that you want to see in plugin menu
+    //            PFUNCPLUGINCMD functionPointer, // the symbol of function (function pointer) associated with this command. The body should be defined below. See Step 4.
+    //            ShortcutKey *shortcut,          // optional. Define a shortcut to trigger this command
+    //            bool check0nInit                // optional. Make this menu item be checked visually
+    //            );
+
+
+	// Shortcut :
+	// Following code makes the first command
+	// bind to the shortcut Alt-F
+	ShortcutKey *shKey = new ShortcutKey;
+	shKey->_isAlt = true;
+	shKey->_isCtrl = false;
+	shKey->_isShift = false;
+	shKey->_key = 0x44; //VK_R
+
+
+	setCommand(0, TEXT("Prefrences"), showPrefDialog, NULL, false);
+	setCommand(1, TEXT("About"), showAboutDialog, NULL, false);
+	setCommand(2, TEXT("Toogle rtl"), toggleRtl, shKey, false);
+
+}
+
+
+//
+// Here you can do the clean up (especially for the shortcut)
+//
+void commandMenuCleanUp()
+{
+	// Don't forget to deallocate your shortcut here
+
+}
+
+//----------------------------------------------//
+//-- STEP 4. DEFINE YOUR ASSOCIATED FUNCTIONS --//
+//----------------------------------------------//
+
+
+//is function help you to initialize your plugin commands
+//
+bool setCommand(size_t index, TCHAR *cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey *sk, bool check0nInit) 
+{
+    if (index >= nbFunc)
+        return false;
+
+    if (!pFunc)
+        return false;
+
+    lstrcpy(funcItem[index]._itemName, cmdName);
+    funcItem[index]._pFunc = pFunc;
+    funcItem[index]._init2Check = check0nInit;
+    funcItem[index]._pShKey = sk;
+
+    return true;
+}
+
+
